d10eb4f530bfae71778baac5a78cd00fae8e44b9
RoiNeuman <roi.neuman@gmail.com>
2020-05-13 15:51:02 +0300

    Fix MagicList class

--- src/magic_list.py
+++ src/magic_list.py
@@ -1,27 +1,21 @@
-from typing import List
 
 
 class MagicList(list):
 
     def __init__(self, cls_type: type = None):
         self.cls_type = cls_type
-        if cls_type is None:
-            self._list = []
-        else:
-            self._list: List[cls_type] = []
-        list.__init__(self._list)
-
-    def __len__(self):
-        return len(self._list)
+        list.__init__(self)
 
     def __getitem__(self, y):
-        return self._list[y]
-
-    def __call__(self):
-        return self._list
+        if len(self) == y and self.cls_type is not None:
+            self.append(self.cls_type())
+        return super().__getitem__(y)
 
     def __setitem__(self, key, value):
-        if len(self._list) == key:
-            self._list.append(value)
+        if len(self) == key:
+            if self.cls_type is not None:
+                self.append(self.cls_type())
+            else:
+                self.append(value)
         else:
-            self._list[key] = value
+            self[key] = value
